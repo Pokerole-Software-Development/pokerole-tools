6a3f4f1dff87cb3bd3173c7392890d432996b2cc
SirIntellegence <SirIntellegence@users.noreply.github.com>
2021-01-11 21:39:38 -0700

    We now have a semblence of source code generated
    Doesn't build though. Still not done with it.

---
+++ pokerole-core/DataItems.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokerole.Core
+{
+	public abstract record BaseDataItem
+	{
+		private readonly DataId dataId;
+		public DataId DataId => dataId;
+		protected BaseDataItem(DataId id)
+		{
+			dataId = id;
+		}
+	}
+	public interface IEffect { }
+	public class ImageRef { }
+	public record Height { }
+	public record Weight { }
+	class DataItems
+	{
+	}
+}
