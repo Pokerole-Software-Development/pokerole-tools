414a81b25733588fab2df1e690e5d904f7d9c927
SirIntellegence <SirIntellegence@users.noreply.github.com>
2022-01-01 17:24:34 -0700

    Implementing basic UI for checking entries.
    NOT FOR PRIMARY USE!

---
+++ DebugDexViewer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace DebugDexViewer.ViewModels
+{
+	public class ViewModelBase : ReactiveObject
+	{
+	}
+}
